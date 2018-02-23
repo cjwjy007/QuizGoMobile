1a3ba927237d74103f0c9a33bf8da85a63a249fe
wei <317694683@qq.com>
2018-02-23 13:46:54 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -26,5 +26,11 @@ new Vue({
     if (this.$store.state.auth.username === '') {
       this.$router.push({name: 'UserInput'})
     }
+  },
+  beforeEnter(to, from, next) {
+    if (from.path !== '/') {
+      router.go(to.path)
+    }
+    next()
   }
 })
